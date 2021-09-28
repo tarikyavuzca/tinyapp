60fddedd987363a79f0d357698ff6ba778d775ba
tarikyavuzca <tarikyavuzca@gmail.com>
2021-09-27 21:04:01 -0400

    ejs file is added

--- express_server.js
+++ express_server.js
@@ -4,12 +4,19 @@ const PORT = 8080; //default port 8080
 
 app.set("view engine", "ejs");
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+
+// **********************************************************************************
 app.get("/", (req, res) =>  {
   res.send("Hello!");
 });
