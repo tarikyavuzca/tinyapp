0971d04dc35731aa98631bed8fd143c6051dc550
tarikyavuzca <tarikyavuzca@gmail.com>
2021-09-27 21:18:10 -0400

    adding urls_show file

--- express_server.js
+++ express_server.js
@@ -14,6 +14,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 
 
 // **********************************************************************************
