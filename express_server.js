88146b160da1b18e4834255f2fa067adf808ed33
tarikyavuzca <tarikyavuzca@gmail.com>
2021-09-29 23:09:06 -0400

    Cleaning the code and add some comment

--- express_server.js
+++ express_server.js
@@ -3,8 +3,9 @@ const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 const app = express();
 
-const PORT = 8080; //default port 8080
+const PORT = 8080; 
 
+// generateRandomString function added to generate userId and short url
 const generateRandomString = function() {
   let randomString = "";
   randomString = Math.random().toString(36).substr(2,6);
@@ -22,34 +23,35 @@ const createUser = function (email, password, users) {
   return userId;
 };
 
-
+// duplicateEmail function accepting email argument as parameter, created to maku sure that the user type email address that not in use.
 const duplicateEmail = function(email) {
-  for (let user in users) {
-    if (users[user].email === email) {
-      return users[user].id;
+  for (let user in users) { //looping through the users object
+    if (users[user].email === email) { // if the email that user type is in the database
+      return users[user].id; // return that email otherwise return false
     }
   }
   return false;
 };
 
+// findUserByEmail function accepting email as parameter, created to find the user with the email address in the database
 const findUserByEmail = function (email) {
-  for (let id in users) {
-    const user = users[id];
-    if(email === user.email) {
-      return user;
+  for (let id in users) { // looping through the users object
+    const user = users[id]; // creating user variable and assign it to usersid
+    if(email === user.email) { // if the email address matches with the one in db
+      return user; // return that user otherwise return false
     }
   }
   return false;
 };
 
-const authenticateUser = function (email, password, users) {
-  const userFound = findUserByEmail(email, users);
+// const authenticateUser = function (email, password, users) {
+//   const userFound = findUserByEmail(email, users);
 
-  if (userFound && userFound.password === password) {
-    return userFound;
-  };
-  return false;
-};
+//   if (userFound && userFound.password === password) {
+//     return userFound;
+//   };
+//   return false;
+// };
 
 const usersUrls = function(id) {
   const userUrls = {};
